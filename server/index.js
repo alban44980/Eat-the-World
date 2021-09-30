76b525038aef79c9b7142bf2bcf2824eea483f32
Alban Mansord <alban.mansord@gmail.com>
2021-09-30 16:08:50 +0200

    first commit from Alban-dev

--- server/index.js
+++ server/index.js
@@ -4,17 +4,12 @@ const cors = require('cors');
 const router = require('./router');
 const PORT = 3002;
 
-
-//cors
 app.use(cors());
 
-//parser
 app.use(express.json());
 
-//router
 app.use(router);
 
-//listen
 app.listen(PORT, (err) => {
   if (err) return console.log(err);
   console.log('Server is bloody running!');
