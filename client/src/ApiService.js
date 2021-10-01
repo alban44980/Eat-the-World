27260aa17455d91f15c16b80219b3056c518d0a7
Alban Mansord <alban.mansord@gmail.com>
2021-10-01 16:30:40 +0200

    refactored ApiService

--- client/src/ApiService.js
+++ client/src/ApiService.js
@@ -13,21 +13,13 @@ apiService.getDishImage = (dish) => {
 };
 
 apiService.getDishInfo = (dishSelected) => {
-  return (
-    fetch('http://localhost:3002/info', {
-      method: 'POST',
-      headers: { 'content-type': 'application/json' },
-      body: JSON.stringify({ dish: dishSelected }),
-    })
-      .then((res) => res.json())
-      // .then((data) => console.log(data))
-      .catch((err) => console.log(err))
-  );
+  return fetch('http://localhost:3002/info', {
+    method: 'POST',
+    headers: { 'content-type': 'application/json' },
+    body: JSON.stringify({ dish: dishSelected }),
+  })
+    .then((res) => res.json())
+    .catch((err) => console.log(err));
 };
 
 export default apiService;
-
-//commited Alban-testing
-// git checkout commitId
-
-//THIS HAS BEEN SAVED!!!
