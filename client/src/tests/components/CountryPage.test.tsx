06b2e3fe38bd3e018b3c18a5346a4e49ca9fc3cd
Will Sukonik <will.sukonik@icloud.com>
2021-10-04 15:04:19 +0200

    not sure what this is tbh

---
+++ client/src/tests/components/CountryPage.test.tsx
@@ -0,0 +1,31 @@
+import CountryPage from '../../components/CountryPage';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import React from 'react';
+
+// countrySelected,
+// SetSelectedDish,
+// updateFavorites,
+// favorites,
+
+
+// it('should call onDishClick', () => {
+//   const countrySelected = 'Greenland'
+//   const SetSelectedDish = jest.fn();
+//   const dishSelected = 'Kiviak';
+//   const favorites = ['Fish and Chips'];
+
+//   render(
+//     <BrowserRouter>
+//       <CountryPage
+//         countrySelected={countrySelected}
+//         SetSelectedDish={SetSelectedDish}
+//         dishSelected={dishSelected}
+//         favorites={favorites}
+//       />
+//     </BrowserRouter>
+//   );
+
+//   fireEvent.click(screen.getByTestId('DishSelectButton2'));
+//   expect(SetSelectedDish.mock.calls.length).toBe(1);
+// });
