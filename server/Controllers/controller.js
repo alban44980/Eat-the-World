509fd7da3f07ab38f3c49ca57919783e2ea54e8f
Will Sukonik <will.sukonik@icloud.com>
2021-10-04 15:14:26 +0200

    strucutured backend

--- server/Controllers/controller.js
+++ server/Controllers/controller.js
@@ -6,30 +6,24 @@ exports.getFoodImage = async (req, res) => {
   try {
     const dish = req.body.dish;  
     const imgUrl = await axios.get("http://en.wikipedia.org/w/api.php?action=query&origin=*&prop=pageimages&format=json&piprop=original&titles=" + dish).catch(error => {console.log(error);})
-    
     const imgObj = imgUrl.data.query.pages;
-  
     const imgLink = Object.values(imgObj)[0].original.source;
 
-  
     res.status(200).send({ imgLink });
     res.end();
   } catch (error) {
     res.status(500).send(error); 
   }
 };
-////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 exports.getFoodInfo = async (req, res) => {
     try {
       const dish = req.body.dish;  
       const imgUrl = await axios.get("http://en.wikipedia.org/w/api.php?action=query&origin=*&prop=extracts&format=json&exintro=&titles=" + dish).catch(error => {console.log(error);})
-      
       const imgObj = imgUrl.data.query.pages;
- 
-        const imgLink = Object.values(imgObj)[0].extract;
+      const imgLink = Object.values(imgObj)[0].extract;
       
-      const theDataIWant = "wooo";
       res.status(200).send({ imgLink });
       res.end();
     } catch (error) {
@@ -37,7 +31,6 @@ exports.getFoodInfo = async (req, res) => {
     }
   };
 
-  /////////////////////////////////////////////////
 
   exports.getRestaurants = async (req, res) => {
     try {
@@ -48,6 +41,7 @@ exports.getFoodInfo = async (req, res) => {
       const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&types=restaurant&radius=2000&keyword=${selectedCuisine},&key=${myApi}`;
       const restaurantData = await axios.get(url).catch(error => {console.log(error);})
       const restaurants = restaurantData.data.results;
+      
       res.status(200).send({ restaurants });
       res.end();
     } catch (error) {
